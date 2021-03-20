50e14610e6795a7daab7fb171989c514c9dd397f
carmen <121798996@qq.com>
2021-03-20 11:39:02 +0800

    create project

---
+++ demo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 2021/3/20 11:16
+@Author : Carmen
+@file: demo.py
+"""
+
+
+# demo 上传git
+def add(a, b):
+    c = a + b
+    return c
+
+
+res = add(1, 9)
+print(res)
